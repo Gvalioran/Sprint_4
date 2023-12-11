daf33cb6e64f94e477cbbb50c1f614f349f4acc8
Kirill Seleznev <seleznev97@inbox.ru>
2023-12-12 00:32:23 +0300

    error correction

--- tests.py
+++ tests.py
@@ -3,74 +3,71 @@ from main import BooksCollector
 
 
 class TestBooksCollector:
-    books = [['Гордость и предубеждение и зомби', 'Ужасы'], ['Что делать, если ваш кот хочет вас убить', 'Комедии']]
+    book1 = 'Гордость и предубеждение и зомби'
+    book2 = 'Что делать, если ваш кот хочет вас убить'
+    genre1 = 'Ужасы'
+    genre2 = 'Комедии'
+    books = [[book1, genre1], [book2, genre2]]
 
     def test_add_new_book_add_two_books(self):
         collector = BooksCollector()
-        collector.add_new_book(self.books[0][0])
-        collector.add_new_book(self.books[1][0])
+        collector.add_new_book(self.book1)
+        collector.add_new_book(self.book2)
         assert len(collector.get_books_genre()) == 2
 
-    def test_set_book_genre_set_two_books(self):
+    def test_add_new_book_add_book_more_than_40_characters(self):
         collector = BooksCollector()
-        collector.add_new_book(self.books[0][0])
-        collector.add_new_book(self.books[1][0])
-        collector.set_book_genre(self.books[0][0], self.books[0][1])
-        collector.set_book_genre(self.books[1][0], self.books[1][1])
-        assert collector.books_genre == {self.books[0][0]: self.books[0][1], self.books[1][0]: self.books[1][1]}
+        collector.add_new_book('Странная история доктора Джекила и мистера Хайда (сборник)')
+        assert len(collector.get_books_genre()) == 0
 
-    @pytest.mark.parametrize('book_name, genre', books)
-    def test_get_book_genre_get_book(self, book_name, genre):
+    def test_add_new_book_add_book_no_name(self):
+        collector = BooksCollector()
+        collector.add_new_book('')
+        assert len(collector.get_books_genre()) == 0
+
+    def test_set_books_genre_set_two_books(self):
+        collector = BooksCollector()
+        collector.add_new_book(self.book1)
+        collector.add_new_book(self.book2)
+        collector.set_book_genre(self.book1, self.genre1)
+        collector.set_book_genre(self.book2, self.genre2)
+        assert collector.get_books_genre() == {self.book1: self.genre1, self.book2: self.genre2}
+
+    def test_set_book_genre_set_book_the_missing_genre(self):
+        collector = BooksCollector()
+        collector.add_new_book(self.book1)
+        collector.set_book_genre(self.book1, "Документальный")
+        assert collector.get_book_genre(self.book1) == ''
+
+    @pytest.mark.parametrize('book_name, book_genre', books)
+    def test_get_books_with_specific_genre_get_book_with_specific_genre(self, book_name, book_genre):
         collector = BooksCollector()
         collector.add_new_book(book_name)
-        collector.set_book_genre(book_name, genre)
-        assert collector.get_book_genre(book_name) == genre
-
-    @pytest.mark.parametrize('book_name, genre', books)
-    def test_get_books_with_specific_genre_get_book_with_specific_genre(self, book_name, genre):
-        collector = BooksCollector()
-        collector.add_new_book(book_name)
-        collector.set_book_genre(book_name, genre)
-        assert collector.get_books_with_specific_genre(genre) == [book_name]
-
-    def test_get_books_genre_get_two_books(self):
-        collector = BooksCollector()
-        collector.add_new_book(self.books[0][0])
-        collector.add_new_book(self.books[1][0])
-        collector.set_book_genre(self.books[0][0], self.books[0][1])
-        collector.set_book_genre(self.books[1][0], self.books[1][1])
-        assert collector.get_books_genre() == {self.books[0][0]: self.books[0][1], self.books[1][0]: self.books[1][1]}
+        collector.set_book_genre(book_name, book_genre)
+        assert collector.get_books_with_specific_genre(book_genre) == [book_name]
 
     def test_get_books_for_children_get_two_books(self):
         collector = BooksCollector()
-        collector.add_new_book(self.books[0][0])
-        collector.add_new_book(self.books[1][0])
-        collector.set_book_genre(self.books[0][0], self.books[0][1])
-        collector.set_book_genre(self.books[1][0], self.books[1][1])
-        assert collector.get_books_for_children() == [self.books[1][0]]
+        collector.add_new_book(self.book1)
+        collector.add_new_book(self.book2)
+        collector.set_book_genre(self.book1, self.genre1)
+        collector.set_book_genre(self.book2, self.genre2)
+        assert collector.get_books_for_children() == [self.book2]
 
     def test_add_book_in_favorites_add_two_books(self):
         collector = BooksCollector()
-        collector.add_new_book(self.books[0][0])
-        collector.add_new_book(self.books[1][0])
-        collector.add_book_in_favorites(self.books[0][0])
-        collector.add_book_in_favorites(self.books[1][0])
-        assert len(collector.favorites) == 2
+        collector.add_new_book(self.book1)
+        collector.add_new_book(self.book2)
+        collector.add_book_in_favorites(self.book1)
+        collector.add_book_in_favorites(self.book2)
+        assert len(collector.get_list_of_favorites_books()) == 2
 
     def test_delete_book_from_favorites_delete_two_books(self):
         collector = BooksCollector()
-        collector.add_new_book(self.books[0][0])
-        collector.add_new_book(self.books[1][0])
-        collector.add_book_in_favorites(self.books[0][0])
-        collector.add_book_in_favorites(self.books[1][0])
-        collector.delete_book_from_favorites(self.books[0][0])
-        collector.delete_book_from_favorites(self.books[1][0])
-        assert len(collector.favorites) == 0
-
-    def test_get_list_of_favorites_books_get_two_books(self):
-        collector = BooksCollector()
-        collector.add_new_book(self.books[0][0])
-        collector.add_new_book(self.books[1][0])
-        collector.add_book_in_favorites(self.books[0][0])
-        collector.add_book_in_favorites(self.books[1][0])
-        assert collector.get_list_of_favorites_books() == [self.books[0][0], self.books[1][0]]
+        collector.add_new_book(self.book1)
+        collector.add_new_book(self.book2)
+        collector.add_book_in_favorites(self.book1)
+        collector.add_book_in_favorites(self.book2)
+        collector.delete_book_from_favorites(self.book1)
+        collector.delete_book_from_favorites(self.book2)
+        assert len(collector.get_list_of_favorites_books()) == 0
